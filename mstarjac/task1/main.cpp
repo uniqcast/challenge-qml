beff5b4e1a4c126f0d267a2c7c380f5875b744ec
mstarjac <mstarjac@gmail.com>
2016-10-15 14:54:42 +0200

    task1 done

---
+++ mstarjac/task1/main.cpp
@@ -0,0 +1,13 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QtQuickControls2>
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+
+    return app.exec();
+}
